c754d7b89529721f371d6078ff6e7efd2606e6aa
fox <102655383+f0xb17@users.noreply.github.com>
2024-12-17 21:15:15 +0100

    Create theme.lua
    Added theme for awesome

---
+++ config/awesome/theme.lua
@@ -0,0 +1,32 @@
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+local theme = {}
+
+theme.font          = "sans 8"
+
+theme.bg_normal     = "#222222"
+theme.bg_focus      = "#535d6c"
+theme.bg_urgent     = "#ff0000"
+theme.bg_minimize   = "#444444"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#aaaaaa"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
+
+theme.useless_gap   = dpi(5)
+theme.gap_single_client = true
+theme.border_width  = dpi(4)
+theme.border_normal = "#000000"
+theme.border_focus  = "#535d6c"
+theme.border_marked = "#91231c"
+
+theme.wallpaper = themes_path.."default/background.png"
+
+return theme
