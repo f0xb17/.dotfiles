735d289fd7714ace03a0cf86ceb64b96a91c4e69
fox <102655383+f0xb17@users.noreply.github.com>
2024-12-20 12:26:18 +0100

    Update autorun.sh
    Added dbus

--- config/scripts/autorun.sh
+++ config/scripts/autorun.sh
@@ -7,6 +7,8 @@ picom --config ~/.config/picom/picom.conf & disown
 polybar &
 dunst &
 
+dbus-update-activation-environment --all & disown
+
 gnome-keyring-daemon --start --components=ssh,secrets,pkcs11,login & disown
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1  & disown
